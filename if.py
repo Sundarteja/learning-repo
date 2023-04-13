91f71fd198f69e61f51acc5465cb409b6efd48ad
Sundarteja <skurapat@uab.edu>
2023-04-12 21:57:56 -0500

    if.py

---
+++ if.py
@@ -0,0 +1,9 @@
+import datetime 
+#print(datetime.date.today().day)
+check = datetime.date.today().day
+if(check > 1):
+    print("Its due past date that is First")
+elif(check == 1):
+    print("Exact Match")
+else:
+    print("not satified condition")
